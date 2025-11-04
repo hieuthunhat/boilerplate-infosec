477971d76fa103dad7c16062551e2e77e9a5f43c
hieunt15 <hieunt1@avadagroup.com>
2025-11-04 14:45:40 +0700

    add hsts

--- myApp.js
+++ myApp.js
@@ -8,9 +8,9 @@ app.use(helmet.frameguard({action: 'deny'}));
 app.use(helmet.xssFilter());
 app.use(helmet.noSniff());
 app.use(helmet.ieNoOpen());
+app.use(helmet.hsts({maxAge: 90*24*60*60}));
 
-
-
+  
 
 
 
